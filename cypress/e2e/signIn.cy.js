407ba44f33e1acda09483d5a083cebbe3c9ac004
Piotr Kruszelnicki <arbuzzz1337@gmail.com>
2025-07-07 17:01:53 +0200

    Cypress authentication

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,11 +1,51 @@
 /// <reference types="cypress" />
 
-describe('Sign In page', () => {
-  beforeEach(() => {
+describe('Login/Logout flow', () => {
+  it('should provide an ability to log in', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+
+    cy.url().should('include', '/secure');
   });
 
-  it('', () => {
+  it('should not provide an ability to log in with invalid credentials', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
 
+    cy.get('#username')
+      .type('asd');
+    cy.get('#password')
+      .type('asd');
+    cy.get('.fa')
+      .click();
+
+    cy.get('.flash.error')
+      .should('be.visible')
+      .and('contain', 'Your username is invalid!');
+  });
+
+  it('should logout from the app', () => {
+    cy.visit('https://the-internet.herokuapp.com/login');
+
+    cy.get('#username')
+      .type('tomsmith');
+    cy.get('#password')
+      .type('SuperSecretPassword!');
+    cy.get('.fa')
+      .click();
+
+    cy.url().should('include', '/secure');
+
+    cy.contains('a', 'Logout')
+      .click();
+
+    cy.get('.flash.success')
+      .should('be.visible')
+      .and('contain', 'You logged out of the secure area!');
   });
 });
